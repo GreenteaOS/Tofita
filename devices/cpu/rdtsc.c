669214746e3359632041288d4c92d79a74d7abe7
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2019-01-22 07:28:02 +0300

    RDTSC timer

---
+++ devices/cpu/rdtsc.c
@@ -0,0 +1,22 @@
+// The Tofita Kernel
+// Copyright (C) 2019  Oleg Petrenko
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Read the RDTSC timer
+
+uint64_t __attribute__((fastcall)) rdtscTicks() {
+	uint32_t a, d;
+	asm volatile("rdtsc" : "=a" (a), "=d" (d));
+	return (((uint64_t)a) | (((uint64_t)d) << 32));
+}
