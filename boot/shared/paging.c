25978bf121ab290624e6eb854c69d84eef6c2271
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2019-01-26 02:43:46 +0300

    Stay with LGPLv3 and finally fill all notices

--- boot/shared/paging.c
+++ boot/shared/paging.c
@@ -1,3 +1,18 @@
+// The Tofita Kernel
+// Copyright (C) 2019  Oleg Petrenko
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #define PAGE_SIZE 4096  // 4 KiB
 #define ADDRESS_BITS 12
 #define PAGE_ALIGNED __attribute__((aligned(PAGE_SIZE)))
