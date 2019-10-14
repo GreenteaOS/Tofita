28e99444dabf36d9d6ad213ef1f948dbe8414cd0
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2019-10-14 16:24:49 -0700

    Exiting on error

--- build.bat
+++ build.bat
@@ -1,6 +1,14 @@
 @echo off
 cmd /c hexa build\hexa.json
+IF %ERRORLEVEL% NEQ 0 (
+  echo "[Exiting on error]"
+  exit
+)
 node build.js build
+IF %ERRORLEVEL% NEQ 0 (
+  echo "[Exiting on error]"
+  exit
+)
 ::node build.js vbox
 ::node build.js qemu
 ::node tools\listen.js
