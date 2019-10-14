72877ad4f260abe0804526f128a8161e8c1196ec
PeyTy <3642643+PeyTy@users.noreply.github.com>
2019-10-14 11:56:12 -0700

    Change .iso path

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ mcopy -i /mnt/r/tofita/disk.img /mnt/r/TOFITA.DAT ::/TOFITA.DAT
 # Create disk image
 cp -f /mnt/r/TOFITA.DAT /mnt/r/tofita/iso/TOFITA.DAT
 cp -f /mnt/r/tofita/loader.efi /mnt/r/tofita/iso/EFI/BOOT/BOOTX64.EFI
-xorriso -as mkisofs -o /mnt/r/tofita/uefi.iso -iso-level 3 -V UEFI /mnt/r/tofita/iso \
+xorriso -as mkisofs -o /mnt/r/tofita/greenteaos-uefi.iso -iso-level 3 -V UEFI /mnt/r/tofita/iso \
 	-append_partition 2 0xef /mnt/r/tofita/disk.img \
 	-e --interval:appended_partition_2:all:: -no-emul-boot
 
