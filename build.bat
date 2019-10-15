3b6cfa9a3a37a5e2e6a68bc0f29f498e986dc7c4
PeyTy <3642643+PeyTy@users.noreply.github.com>
2019-10-14 17:31:54 -0700

    Better batch script

--- build.bat
+++ build.bat
@@ -1,16 +1,23 @@
 @echo off
+color 0B
+echo [Welcome to Tofita!]
+cd %~dp0
 cmd /c hexa build\hexa.json
 IF %ERRORLEVEL% NEQ 0 (
-  echo "[Exiting on error]"
-  exit
+  goto somethingbad
 )
 node build.js build
 IF %ERRORLEVEL% NEQ 0 (
-  echo "[Exiting on error]"
-  exit
+  goto somethingbad
 )
+color 0A
+echo [Success]
 ::node build.js vbox
 ::node build.js qemu
 ::node tools\listen.js
 ::node tools\pipe.js
-exit
+goto done
+:somethingbad
+color 0C
+echo [Exiting on error]
+:done
