f443d6121499ed15e236f460b0a715ccf0f8f5f3
PeyTy <3642643+PeyTy@users.noreply.github.com>
2019-10-21 12:51:37 -0700

    Call mtools from cmd

--- iso.sh
+++ iso.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # Create floppy image
-cp ../Teapot/mtools/disk-template.img /mnt/r/tofita/disk.img
-../Teapot/mtools/mmd.exe -i R:\\tofita\\disk.img ::/EFI
-../Teapot/mtools/mmd.exe -i R:\\tofita\\disk.img ::/EFI/BOOT
-../Teapot/mtools/mcopy.exe -i R:\\tofita\\disk.img R:\\tofita\\loader.efi ::/EFI/BOOT/BOOTx64.EFI
-../Teapot/mtools/mcopy.exe -i R:\\tofita\\disk.img R:\\tofita\\iso\\TOFITA.DAT ::/TOFITA.DAT
 
 # Create disk image
 xorriso -as mkisofs -o /mnt/r/tofita/greenteaos-uefi.iso -iso-level 3 -V UEFI /mnt/r/tofita/iso \
