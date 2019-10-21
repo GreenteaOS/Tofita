ac4dddb5af2d99278b9ccca23145ffefd53ddabf
PeyTy <3642643+PeyTy@users.noreply.github.com>
2019-10-21 07:12:10 -0700

    Save assets directly into .iso

--- iso.sh
+++ iso.sh
@@ -6,10 +6,9 @@ mkfs.vfat /mnt/r/tofita/disk.img
 mmd -i /mnt/r/tofita/disk.img ::/EFI
 mmd -i /mnt/r/tofita/disk.img ::/EFI/BOOT
 mcopy -i /mnt/r/tofita/disk.img /mnt/r/tofita/loader.efi ::/EFI/BOOT/BOOTx64.EFI
-mcopy -i /mnt/r/tofita/disk.img /mnt/r/TOFITA.DAT ::/TOFITA.DAT
+mcopy -i /mnt/r/tofita/disk.img /mnt/r/tofita/iso/TOFITA.DAT ::/TOFITA.DAT
 
 # Create disk image
-cp -f /mnt/r/TOFITA.DAT /mnt/r/tofita/iso/TOFITA.DAT
 xorriso -as mkisofs -o /mnt/r/tofita/greenteaos-uefi.iso -iso-level 3 -V UEFI /mnt/r/tofita/iso \
 	-append_partition 2 0xef /mnt/r/tofita/disk.img \
 	-e --interval:appended_partition_2:all:: -no-emul-boot
