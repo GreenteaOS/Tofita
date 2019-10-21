bed04623b52b35e41c28268782d1d1ffc2ad4319
PeyTy <3642643+PeyTy@users.noreply.github.com>
2019-10-21 09:18:02 -0700

    Use mtools from Teapot

--- iso.sh
+++ iso.sh
@@ -3,10 +3,10 @@
 # Create floppy image
 dd if=/dev/zero of=/mnt/r/tofita/disk.img bs=1k count=6272
 mkfs.vfat /mnt/r/tofita/disk.img
-mmd -i /mnt/r/tofita/disk.img ::/EFI
-mmd -i /mnt/r/tofita/disk.img ::/EFI/BOOT
-mcopy -i /mnt/r/tofita/disk.img /mnt/r/tofita/loader.efi ::/EFI/BOOT/BOOTx64.EFI
-mcopy -i /mnt/r/tofita/disk.img /mnt/r/tofita/iso/TOFITA.DAT ::/TOFITA.DAT
+../Teapot/mtools/mmd.exe -i R:\\tofita\\disk.img ::/EFI
+../Teapot/mtools/mmd.exe -i R:\\tofita\\disk.img ::/EFI/BOOT
+../Teapot/mtools/mcopy.exe -i R:\\tofita\\disk.img R:\\tofita\\loader.efi ::/EFI/BOOT/BOOTx64.EFI
+../Teapot/mtools/mcopy.exe -i R:\\tofita\\disk.img R:\\tofita\\iso\\TOFITA.DAT ::/TOFITA.DAT
 
 # Create disk image
 xorriso -as mkisofs -o /mnt/r/tofita/greenteaos-uefi.iso -iso-level 3 -V UEFI /mnt/r/tofita/iso \
