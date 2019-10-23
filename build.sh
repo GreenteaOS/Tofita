8a31df73a626d5674952f20fac9a05617445e73c
PeyTy <3642643+PeyTy@users.noreply.github.com>
2019-10-23 06:49:06 -0700

    Use GNU ld

--- build.sh
+++ build.sh
@@ -22,10 +22,11 @@
 	-Iexternal/inc -Iexternal/inc/x86_64 -Iexternal/inc/protocol -DEFI_FUNCTION_WRAPPER \
 	-c -o r:/tofita/tofita.o kernel/tofita.c
 
-ld -T kernel/kernel.ld -o /mnt/r/tofita/tofita_kernel.elf.img \
-	/mnt/r/tofita/tofita.asm.o \
-	/mnt/r/tofita/cpu.asm.o \
-	/mnt/r/tofita/tofita.o
+../Teapot/linux-ubuntu-natty_x86_64-bin_x86_64-mingw_20101218_vityan/bin/x86_64-vityan-linux-gnu-ld.exe -m elf_x86_64 \
+	-T kernel/kernel.ld -o r:/tofita/tofita_kernel.elf.img \
+	r:/tofita/tofita.asm.o \
+	r:/tofita/cpu.asm.o \
+	r:/tofita/tofita.o
 
 ../Teapot/LLVM-9.0.0/bin/llvm-objcopy.exe -O binary r:/tofita/tofita_kernel.elf.img r:/tofita/tofita.img
 cwd=$(pwd)
@@ -33,20 +34,20 @@ cd /mnt/r/tofita/
 $cwd/../Teapot/LLVM-9.0.0/bin/llvm-objcopy.exe -I binary -O elf64-x86-64 -B i386 tofita.img tofitaimg.o
 cd - > /dev/null
 
-ld \
+../Teapot/linux-ubuntu-natty_x86_64-bin_x86_64-mingw_20101218_vityan/bin/x86_64-vityan-linux-gnu-ld.exe -m elf_x86_64 \
 	-T boot/loader/loader.ld \
-	-o /mnt/r/tofita/loader_kernel.elf.img \
-	/mnt/r/tofita/loader.asm.o \
-	/mnt/r/tofita/loader.o \
-	/mnt/r/tofita/tofitaimg.o
+	-o r:/tofita/loader_kernel.elf.img \
+	r:/tofita/loader.asm.o \
+	r:/tofita/loader.o \
+	r:/tofita/tofitaimg.o
 ../Teapot/LLVM-9.0.0/bin/llvm-objcopy.exe -O binary r:/tofita/loader_kernel.elf.img r:/tofita/loader_kernel.img
 
-ld \
+../Teapot/linux-ubuntu-natty_x86_64-bin_x86_64-mingw_20101218_vityan/bin/x86_64-vityan-linux-gnu-ld.exe -m elf_x86_64 \
 	-nostdlib -znocombreloc -T external/gnuefi/elf_x86_64_efi.lds -shared -Bsymbolic -L external/gnuefi \
 	-L /usr/lib \
 	external/gnuefi/crt0-efi-x86_64.o \
-	/mnt/r/tofita/boot.o \
-	-o /mnt/r/tofita/loader.so -lgnuefi
+	r:/tofita/boot.o \
+	-o r:/tofita/loader.so -lgnuefi
 
 ../Teapot/x86_64-w64-mingw32/objcopy.exe \
 	-j .text -j .sdata -j .data -j .dynamic \
