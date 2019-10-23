25802c57d45c0c832aadc927803a411a4a1a046a
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2019-10-23 04:20:30 -0700

    Port CPU code to nasm

--- build.sh
+++ build.sh
@@ -12,8 +12,6 @@ clang-6.0 -O0 -xc -std=gnu11 -fno-stack-protector -fshort-wchar -w -mno-red-zone
 	-Iexternal/inc -Iexternal/inc/x86_64 -Iexternal/inc/protocol -DEFI_FUNCTION_WRAPPER \
 	-c -o /mnt/r/tofita/loader.o boot/loader/loader.c
 
-as -o /mnt/r/tofita/cpu.s.o devices/cpu/cpu.s
-
 clang-6.0 -O2 -xc -std=gnu11 -fno-stack-protector -fshort-wchar -w -mno-red-zone -Wall -Wextra \
 	-Wimplicit-function-declaration -Werror \
 	-DTOFITA -mtune=nocona \
@@ -23,7 +21,7 @@ clang-6.0 -O2 -xc -std=gnu11 -fno-stack-protector -fshort-wchar -w -mno-red-zone
 
 ld -T kernel/kernel.ld -o /mnt/r/tofita/tofita_kernel.elf.img \
 	/mnt/r/tofita/tofita.asm.o \
-	/mnt/r/tofita/cpu.s.o \
+	/mnt/r/tofita/cpu.asm.o \
 	/mnt/r/tofita/tofita.o
 
 ../Teapot/LLVM-9.0.0/bin/llvm-objcopy.exe -O binary r:/tofita/tofita_kernel.elf.img r:/tofita/tofita.img
@@ -32,7 +30,7 @@ cd /mnt/r/tofita/
 $cwd/../Teapot/LLVM-9.0.0/bin/llvm-objcopy.exe -I binary -O elf64-x86-64 -B i386 tofita.img tofitaimg.o
 cd - > /dev/null
 
-ld -T boot/loader/loader.ld -o /mnt/r/tofita/loader_kernel.elf.img /mnt/r/tofita/loader.s.o /mnt/r/tofita/loader.o /mnt/r/tofita/tofitaimg.o
+ld -T boot/loader/loader.ld -o /mnt/r/tofita/loader_kernel.elf.img /mnt/r/tofita/loader.asm.o /mnt/r/tofita/loader.o /mnt/r/tofita/tofitaimg.o
 ../Teapot/LLVM-9.0.0/bin/llvm-objcopy.exe -O binary r:/tofita/loader_kernel.elf.img r:/tofita/loader_kernel.img
 
 ld -nostdlib -znocombreloc -T external/gnuefi/elf_x86_64_efi.lds -shared -Bsymbolic -L external/gnuefi \
---
+++ devices/cpu/cpu.asm
@@ -0,0 +1,132 @@
+; The Tofita Kernel
+; Copyright (C) 2019  Oleg Petrenko
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Lesser General Public License as published by
+; the Free Software Foundation, version 3 of the License.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU Lesser General Public License for more details.
+;
+; You should have received a copy of the GNU Lesser General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+; exports
+global keyboardHandler
+global mouseHandler
+
+global fallback_handler0
+global fallback_handler1
+global fallback_handler2
+global fallback_handler3
+global fallback_handler4
+global fallback_handler5
+global fallback_handler6
+global fallback_handler7
+global fallback_handler8
+global fallback_handler9
+global fallback_handler10
+global fallback_handler11
+global fallback_handler12
+global fallback_handler13
+global fallback_handler14
+global fallback_handler15
+
+;%macro write_string 2
+;	mov eax, 4
+;	mov ebx, 1
+;	mov ecx, %1
+;	mov edx, %2
+;	int 80h
+;%endmacro
+
+%macro save_regs 0
+	push gs
+	push fs
+	push rbp
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rdi
+	push rsi
+	push rdx
+	push rcx
+	push rbx
+	push rax
+	xor  rax, rax
+	mov  ds, ax
+	push rax
+	mov  es, ax
+	push rax
+%endmacro
+
+%macro restore_regs 0
+	pop rax
+	mov ax, es
+	pop rax
+	mov ax, ds
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rsi
+	pop rdi
+	pop r8
+	pop r9
+	pop r10
+	pop r11
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+	pop rbp
+	pop fs
+	pop gs
+%endmacro
+
+%macro fallback 1
+	fallback_handler%1:
+		save_regs
+		extern handle_fallback%1
+		call handle_fallback%1
+		restore_regs
+		iretq
+%endmacro
+
+keyboardHandler:
+	save_regs
+	extern handleKeyboard
+	call handleKeyboard
+	restore_regs
+	iretq
+
+mouseHandler:
+	save_regs
+	extern handleMouse
+	call handleMouse
+	restore_regs
+	iretq
+
+fallback 0
+fallback 1
+fallback 2
+fallback 3
+fallback 4
+fallback 5
+fallback 6
+fallback 7
+fallback 8
+fallback 9
+fallback 10
+fallback 11
+fallback 12
+fallback 13
+fallback 14
+fallback 15
