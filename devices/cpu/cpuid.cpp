c3e06ce287d2e437477ebfc1a5a6524d5763b8c5
PeyTy <3642643+PeyTy@users.noreply.github.com>
2019-11-05 19:29:43 -0800

    Use clang-format

--- devices/cpu/cpuid.cpp
+++ devices/cpu/cpuid.cpp
@@ -24,43 +24,43 @@ const uint32_t cpuidExtendedLevels = 0x80000000;
 
 CPUID getCPUID() {
 	CPUID result;
-	amd64::cpuid(0, 0,
-		&result.data,
-		(uint32_t*)(&result.vendorID[0]),
-		(uint32_t*)(&result.vendorID[8]),
-		(uint32_t*)(&result.vendorID[4]));
+	amd64::cpuid(0, 0, &result.data, (uint32_t *)(&result.vendorID[0]),
+				 (uint32_t *)(&result.vendorID[8]),
+				 (uint32_t *)(&result.vendorID[4]));
 
 	amd64::cpuid(cpuidExtendedLevels, 0, &result.extras);
 
 	if (result.extras >= cpuidExtendedLevels + 4) {
 		amd64::cpuid(cpuidExtendedLevels + 2, 0,
-			(uint32_t*)(&result.brandName[0]),
-			(uint32_t*)(&result.brandName[4]),
-			(uint32_t*)(&result.brandName[8]),
-			(uint32_t*)(&result.brandName[12]));
+					 (uint32_t *)(&result.brandName[0]),
+					 (uint32_t *)(&result.brandName[4]),
+					 (uint32_t *)(&result.brandName[8]),
+					 (uint32_t *)(&result.brandName[12]));
 		amd64::cpuid(cpuidExtendedLevels + 3, 0,
-			(uint32_t*)(&result.brandName[16]),
-			(uint32_t*)(&result.brandName[20]),
-			(uint32_t*)(&result.brandName[24]),
-			(uint32_t*)(&result.brandName[28]));
+					 (uint32_t *)(&result.brandName[16]),
+					 (uint32_t *)(&result.brandName[20]),
+					 (uint32_t *)(&result.brandName[24]),
+					 (uint32_t *)(&result.brandName[28]));
 		amd64::cpuid(cpuidExtendedLevels + 4, 0,
-			(uint32_t*)(&result.brandName[32]),
-			(uint32_t*)(&result.brandName[36]),
-			(uint32_t*)(&result.brandName[40]),
-			(uint32_t*)(&result.brandName[44]));
+					 (uint32_t *)(&result.brandName[32]),
+					 (uint32_t *)(&result.brandName[36]),
+					 (uint32_t *)(&result.brandName[40]),
+					 (uint32_t *)(&result.brandName[44]));
 	}
 
 	auto i = 0;
 	while (i++ < sizeof(result.brandName))
-		if (result.brandName[i] == '\r') result.brandName[i] = 0;
-		else
-		if (result.brandName[i] == '\n') result.brandName[i] = 0;
+		if (result.brandName[i] == '\r')
+			result.brandName[i] = 0;
+		else if (result.brandName[i] == '\n')
+			result.brandName[i] = 0;
 
 	i = 0;
 	while (i++ < sizeof(result.vendorID))
-		if (result.vendorID[i] == '\r') result.vendorID[i] = 0;
-		else
-		if (result.vendorID[i] == '\n') result.vendorID[i] = 0;
+		if (result.vendorID[i] == '\r')
+			result.vendorID[i] = 0;
+		else if (result.vendorID[i] == '\n')
+			result.vendorID[i] = 0;
 
 	return result;
 }
