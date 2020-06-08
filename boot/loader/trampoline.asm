c0895beb283cb59ed5f56f4157ae2247c3942350
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-06-08 06:57:47 -0700

    [BUILD] New EFI loader

--- boot/loader/trampoline.asm
+++ boot/loader/trampoline.asm
@@ -1,5 +1,5 @@
 ; The Tofita Kernel
-; Copyright (C) 2019  Oleg Petrenko
+; Copyright (C) 2020  Oleg Petrenko
 ;
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU Lesser General Public License as published by
@@ -15,6 +15,7 @@
 
 bits 64
 
+; UNIX
 ; rdi - first argument
 ; rsi - second argument
 ; rdx - third argument
@@ -22,6 +23,17 @@ bits 64
 ; r8 - fifth argument
 ; r9 - sixth
 
+; NT
+; rcx - first argument - (uint64_t)params
+; rdx - second argument - (uint64_t)paging::pml4entries (CR3)
+; r8 - third argument - stack
+; r9 - fourth argument - upper (kernel)
+
 cli ; Disable interrupts
-mov cr3, rsi
-o64 jmp rcx
+;mov cr3, rsi ; UNIX
+;o64 jmp rcx ; UNIX
+
+mov cr3, rdx ; NT
+mov rdi, rcx ; NT -> UNIX
+mov rdx, r8 ; NT -> UNIX
+o64 jmp r9 ; NT
