50d586815cfbc46c1eb731ec7fd1f5f4dc0e76f8
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-07-09 15:28:47 -0700

    [DWM] First topmost responder

--- kernel/gui/windows.cpp
+++ kernel/gui/windows.cpp
@@ -64,7 +64,7 @@ function drawShadowBox(int16_t x, int16_t y, uint16_t w, uint16_t h) {
 		}
 }
 
-function drawWindowFrame(const wchar_t *title, int16_t x, int16_t y, uint16_t w, uint16_t h) {
+function drawWindowFrame(const wchar_t *title, int16_t x, int16_t y, uint16_t w, uint16_t h, bool active) {
 	Pixel32 color;
 	color.rgba.r = color.rgba.g = color.rgba.b = 0xAA;
 
@@ -73,7 +73,11 @@ function drawWindowFrame(const wchar_t *title, int16_t x, int16_t y, uint16_t w,
 	drawText(title, x + 8 + 4, y + 9 + 3, color);
 
 	// Frame hairline
-	color.rgba.r = color.rgba.g = color.rgba.b = 164;
+	if (active) {
+		color.color = 0x126099FF;
+	} else {
+		color.rgba.r = color.rgba.g = color.rgba.b = 164;
+	}
 	drawRectangleOutline(color, x, y, w, h);
 
 	// Minimize
