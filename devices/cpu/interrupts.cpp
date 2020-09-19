162be15da4b02b3f51d5c8e4e71fbeacb492ef7f
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-09-18 18:42:32 -0700

    [PS/2] Polling refactor

--- devices/cpu/interrupts.cpp
+++ devices/cpu/interrupts.cpp
@@ -149,11 +149,11 @@ function initializeFallback(IdtEntry *entry, uint64_t fallbackHandler) {
 #define ICW1_LEVEL 0x08		// Level triggered (edge) mode
 #define ICW1_INIT 0x10		// Initialization - required!
 
-#define ICW4_8086 0x01		 // 8086/88 (MCS-80/85) mode
-#define ICW4_AUTO 0x02		 // Auto (normal) EOI
-#define ICW4_BUF_SECONDARY 0x08	 // Buffered mode/secondary
-#define ICW4_BUF_MASTER 0x0C // Buffered mode/primary
-#define ICW4_SFNM 0x10		 // Special fully nested (not)
+#define ICW4_8086 0x01			// 8086/88 (MCS-80/85) mode
+#define ICW4_AUTO 0x02			// Auto (normal) EOI
+#define ICW4_BUF_SECONDARY 0x08 // Buffered mode/secondary
+#define ICW4_BUF_MASTER 0x0C	// Buffered mode/primary
+#define ICW4_SFNM 0x10			// Special fully nested (not)
 
 /*
 arguments:
