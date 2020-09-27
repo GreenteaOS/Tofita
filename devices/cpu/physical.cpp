c8e8ecd8689023c77a5a9fbc4c1470fc48f773e3
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-09-26 20:30:09 -0700

    Update physical.cpp

--- devices/cpu/physical.cpp
+++ devices/cpu/physical.cpp
@@ -30,19 +30,27 @@
 // 4096 >> 12 == 1
 // 8191 >> 12 == 1
 // 8192 >> 12 == 2
+#define KERNEL_RESERVED_PAGES 4096
+// == 16 megabytes
+// ^ this amount of memory cannot be allocated by user process
+// to avoid system crash on our of memory
 
 class PhysicalAllocator {
   private:
 	// Approx. 1 megabyte per each 4 gygabyte of RAM
 	static uint8_t *buffer; // TODO use bits, not bytes
 	static uint64_t count;
+	static uint64_t countUser; // count - KERNEL_RESERVED_PAGES
+	static uint64_t available;
 	static uint64_t last;
 
   public:
+	// static function init(const EfiMemoryMap *memoryMap, uint64_t bufferVirtual, uint64_t ramPages) {
 	static function init(const KernelParams *params) {
 		buffer = (uint8_t *)params->physicalRamBitMaskVirtual;
 		const EfiMemoryMap *memoryMap = &params->efiMemoryMap;
 		count = DOWN_BYTES_TO_PAGES(params->ramBytes);
+		countUser = count - KERNEL_RESERVED_PAGES;
 		last = PHYSICAL_NOT_FOUND;
 
 		memset((void *)buffer, PAGE_RESERVED, count);
@@ -67,6 +75,7 @@ class PhysicalAllocator {
 
 				uint64_t i = where;
 				while (i < steps) {
+					// TODO bounds check - may be not in physycal area
 					buffer[i] = kind;
 					i++;
 				}
@@ -91,7 +100,7 @@ class PhysicalAllocator {
 		}
 
 		{
-			uint64_t available = 0;
+			available = 0;
 			uint64_t i = count;
 			while (i > 0) {
 				i--;
@@ -113,6 +122,12 @@ class PhysicalAllocator {
 
 // Must test as (buffer[_] != PAGE_FREE) == PAGE_RESERVED
 #undef PAGE_RESERVED
+
+	// 31-bit addressable, first 2 GB of RAM
+	// Required by some hardware
+	// We assume this by-default
+	// static uint64_t allocateDmaPage2G() {}
+	// TODO fill pages in reverse so I can traverse bitmap from zero?
 
 	// Returns physical 64-bit address, not page number
 	static uint64_t allocateOnePage() {
@@ -145,6 +160,16 @@ class PhysicalAllocator {
 		return result;
 	}
 
+	// Note: respects KERNEL_RESERVED_PAGES
+	// TODO modify available
+	// TODO each process should have some private extra pages (1 megabyte)
+	// ^ means that available_now = (available - 1mb * process_count)
+	static uint64_t allocateSafeUserspaceOnePagePreZeroed() {
+		if (available > KERNEL_RESERVED_PAGES)
+			return allocateOnePagePreZeroed();
+		return PHYSICAL_NOT_FOUND;
+	}
+
 	// TODO faster
 	static uint64_t allocatePages(uint64_t pages) {
 		// 0 or 1 == 1 page anyway
@@ -164,6 +189,18 @@ class PhysicalAllocator {
 			// TODO ^ same for buffer[]
 			// TODO probably uint32_t is completely enough to represent pages
 			while (i < steps) {
+				// if (buffer[i] != PAGE_FREE) {
+				//	if (current > largest) {
+				//		largest = current;
+				//		largestAt = currentAt;
+				//		if (largest >= pages) break;
+				//	}
+				//	current = 0;
+				//} else {
+				//	if (current == 0) currentAt = i;
+				//	current++;
+				//}
+
 				if (buffer[i] == PAGE_FREE) {
 					if (current == 0)
 						currentAt = i;
@@ -173,6 +210,7 @@ class PhysicalAllocator {
 				if (current >= pages) {
 					largest = current;
 					largestAt = currentAt;
+					// if (largest >= pages)
 					break;
 				}
 
@@ -182,6 +220,8 @@ class PhysicalAllocator {
 
 				i++;
 			}
+			serialPrintf(L"[physical] allocatePages current %u\n", current);
+			serialPrintf(L"[physical] allocatePages currentAt %u\n", currentAt);
 		}
 
 		if (largest >= pages) {
@@ -203,9 +243,23 @@ class PhysicalAllocator {
 		return allocatePages(DOWN_BYTES_TO_PAGES(bytes) + 1);
 	}
 
+	// Current granularity: xxx bytes
+	static uint64_t allocateFileCacheBlock(uint64_t bytes) {
+		// TODO
+		// The idea is to use very fast wide search over bitmask
+		// too quickly find large chunks of memory
+		// for file caches
+		// and quickly deallocate if memory is not enough for apps/kernel
+		// (maybe even in semi-automated way within this memory allocator)
+		// (another very small bitmask for disk caches?)
+		return allocatePages(DOWN_BYTES_TO_PAGES(bytes) + 1);
+	}
+
 	static function freeOnePage(uint64_t physical) {
-		physical -= (uint64_t)WholePhysicalStart;
-		let where = DOWN_BYTES_TO_PAGES(physical);
+		let where = DOWN_BYTES_TO_PAGES(physical - (uint64_t)WholePhysicalStart);
+		// Bounds check
+		if (physical <= WholePhysicalStart || where > count)
+			return;
 		// Cosmic rays
 		if (buffer[where] == PAGE_FULL)
 			buffer[where] = PAGE_FREE;
@@ -221,6 +275,19 @@ class PhysicalAllocator {
 		// TODO
 		freePages(physical, DOWN_BYTES_TO_PAGES(bytes) + 1);
 	}
+
+	// Same mechanics as in garbage collector
+	static function freeIfNotUsed(uint64_t physical, uint64_t bytes) {
+		// TODO
+	}
+
+	// Same mechanics as in garbage collector
+	// Applies only on whole pages
+	// Pointers should be 64-bit aligned
+	static function freeAllNotUsedPages() {
+		// TODO
+		// TODO return amount of freed pages
+	}
 };
 
 #undef PAGE_FULL
@@ -229,6 +296,8 @@ class PhysicalAllocator {
 uint8_t *PhysicalAllocator::buffer;
 uint64_t PhysicalAllocator::count;
 uint64_t PhysicalAllocator::last;
+uint64_t PhysicalAllocator::available;
+uint64_t PhysicalAllocator::countUser;
 
 uint64_t allocateBytes(uint64_t bytes) {
 	return PhysicalAllocator::allocateBytes(bytes);
