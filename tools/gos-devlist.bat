8c488ba221a99c1ca1087c6362ca1ffd8e0865a0
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2021-05-30 02:39:38 +0300

    Update gos-devlist.bat

--- tools/gos-devlist.bat
+++ tools/gos-devlist.bat
@@ -1,3 +1,8 @@
 @echo off
 color 0A
+wmic cpu get name
+wmic baseboard get product
+wmic sounddev get name,PNPDeviceID
+wmic PATH Win32_VideoController GET Caption,PNPDeviceID
+wmic nic where "PNPDeviceID like '%%PCI%%'" get name,PNPDeviceID
 pause
