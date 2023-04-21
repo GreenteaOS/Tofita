4464ee21a699b866356726c43f2cfb60c38d8f27
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-04-21 06:50:14 +0300

    [Refactor] Notes, comments

--- build.bat
+++ build.bat
@@ -2,12 +2,17 @@
 echo [32mWelcome to the Greentea OS! Support us at https://www.patreon.com/PeyTy and https://greenteaos.github.io/donate/
 echo [0m
 cd /d %~dp0
+::TODO if not exists Greentea You seems not downloaded repo... Can be downloaded at url
+::TODO if not exists Teapot You seems not downloaded repo...
+::TODO hexa--is-present , if err code get hexa at url
 
 :: Build the builder itself
 cmd /c hexa build\hexa.json
 IF %ERRORLEVEL% NEQ 0 (
   goto somethingbad
 )
+:: TODO ! hexa --deps(name+date)path.txt to print for deps while compile, so
+:: we can compare dates for cashes and rebuild only required projects
 
 ::debug
 node build.js init-or-clean asm efi dll kernel ramdisk iso
