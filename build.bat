1cd7d1fcd636c9bd1200d2f95545599c899d768c
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-06-19 13:23:57 +0300

    [BUILD] Power profiles for faster builds

--- build.bat
+++ build.bat
@@ -6,6 +6,9 @@ cd /d %~dp0
 ::TODO if not exists Teapot You seems not downloaded repo...
 ::TODO hexa--is-present , if err code get hexa at url
 
+:: Balanced
+:: powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e
+
 :: Build the builder itself
 cmd /c hexa build\hexa.json
 IF %ERRORLEVEL% NEQ 0 (
@@ -27,6 +30,10 @@ ATTRIB +S .
 ::tools\qemu-vfat.bat
 ::cd /d "C:\Program Files\Oracle\VirtualBox"
 ::VBoxManage startvm "Tofita"
+
+:: Eco
+:: powercfg /setactive 4353c124-51fe-4f9b-b6b2-0d3f94691a20
+
 ::node build.js vbox
 ::node tools\vbox.js
 ::node tools\listen.js
