7829a7dc66a9a4eb25e495c09bbfe96f0d58274d
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-12-22 06:02:17 +0200

    [CPU] Set CR4 flags

--- boot/loader/trampoline.asm
+++ boot/loader/trampoline.asm
@@ -41,33 +41,45 @@ section .head
 
 global trampolineCR3
 trampolineCR3:
+	cli ; Disable interrupts
 
-cli ; Disable interrupts
+	; TODO conditionally enable OSXSAVE when XSAVE is in CPUID
+	; Same as in trapeze.asm
+	; TODO export to cr4.asm and include it in trapeze.asm too
+	xor rax, rax
+	mov rax, cr4
+	;or rax, 1 << 18 ; Enable OSXSAVE
+	or rax, 1 << 10 ; Unmasked SSE exceptions
+	;or rax, 1 << 9 ; FXSAVE/FXRSTOR
+	or rax, 1 << 7 ; Page Global
+	or rax, 1 << 5 ; Page Address Extension
+	or rax, 1 << 4 ; Page Size Extension
+	mov cr4, rax
 
-mov cr3, rdx ; Tofita upper-half paging
+	mov cr3, rdx ; Tofita upper-half paging
 
-mov rsp, r8
-push 0 ; Signal end of stack with 0 return address
-push 0 ; and a few extra entries in case of stack
-push 0 ; problems
-push 0
-mov rbp, rsp ; Frame
+	mov rsp, r8
+	push 0 ; Signal end of stack with 0 return address
+	push 0 ; and a few extra entries in case of stack
+	push 0 ; problems
+	push 0
+	mov rbp, rsp ; Frame
 
-; TODO Reset state
-;cld ; Clear direction flag
-;; Clear registers
-;xor rax, rax
-;xor rdx, rdx
-;xor rdi, rdi
-;xor rsi, rsi
-;xor r8, r8
-;xor xmm0, xmm0
-;xor xmm1, xmm1
-;xor xmm2, xmm2
-;xor xmm3, xmm3
+	; TODO Reset state
+	;cld ; Clear direction flag
+	;; Clear registers
+	;xor rax, rax
+	;xor rdx, rdx
+	;xor rdi, rdi
+	;xor rsi, rsi
+	;xor r8, r8
+	;xor xmm0, xmm0
+	;xor xmm1, xmm1
+	;xor xmm2, xmm2
+	;xor xmm3, xmm3
 
-; Enter main [with rcx]
-o64 call r9
+	; Enter main [with rcx]
+	o64 call r9
 
 ; COM write
 global portOutb
