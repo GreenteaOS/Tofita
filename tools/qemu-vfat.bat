bd542d65c89dfabe28b625431d903e0b0f6ed77b
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-12-26 22:43:04 +0200

    Update qemu-vfat.bat
    Try R: first and then C:

--- tools/qemu-vfat.bat
+++ tools/qemu-vfat.bat
@@ -1,17 +1,29 @@
 @echo off
 color 0A
+setlocal enableDelayedExpansion
+cls
 
-:: You must set proper paths to run this
+:: Try R: first and then C:
 
-set DRIVE=C
+set DRIVE=R
+set WAY=\Tea\greenteaos-uefi64.iso
+set ISO=%DRIVE%:%WAY%
+
+if not exist "%ISO%" (
+	echo Trying alternative path...
+	set DRIVE=C
+	set ISO=!DRIVE!:!WAY!
+	echo !ISO!
+)
+
 set QEMU="C:\Program Files\qemu\qemu-system-x86_64.exe"
 set OVMF="..\Teapot\OVMF.fd"
 
 %QEMU% -accel tcg -bios %OVMF% -m 2048 ^
     -vga std -machine pc-q35-2.10 ^
-    -serial file:%DRIVE%:\Tea\qemu.log -cpu max,x2apic=off ^
+    -serial file:!DRIVE!:\Tea\qemu.log -cpu max,x2apic=off ^
     -smp 4 ^
-    -cdrom %DRIVE%:\Tea\greenteaos-uefi64.iso ^
+    -cdrom !ISO! ^
     -name "Greentea QEMU" -monitor stdio
 ::    -drive format=raw,file=fat:rw:%DRIVE%:\Tea\spin-off ^
 ::pause
